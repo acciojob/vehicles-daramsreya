14ada880612ecb08b5c03a9bda45701bfe3dfb58
daramsreya83@gmail.com <your.email@example.com>
2025-05-22 20:37:16 +0530

    Vechiles Assignement

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,5 +6,9 @@ public class Main {
         mercedes.move(40, 0);
         mercedes.accelerate(20);
         mercedes.accelerate(-60);
+
+        Boat ferry = new Boat("Ferry King", 100);
+        System.out.println("Boat Name: " + ferry.getVehicleName());
+        System.out.println("Boat Capacity: " + ferry.getVehicleCapacity());
     }
 }
